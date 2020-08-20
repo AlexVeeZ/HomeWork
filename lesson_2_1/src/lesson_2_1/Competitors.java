79245f3b778a9375989c26d32b74564d9035bb0a
AlexVee <alexveez@yandex.ru>
2020-08-20 11:25:59 +0300

    lesson_2_1

---
+++ lesson_2_1/src/lesson_2_1/Competitors.java
@@ -0,0 +1,8 @@
+package lesson_2_1;
+
+public interface Competitors {
+
+    void runDistance(RunningTrack runningTrack);
+    void jumpTheWall(Wall wall);
+
+}
---
+++ lesson_2_1/src/lesson_2_1/RunningTrack.java
@@ -0,0 +1,16 @@
+package lesson_2_1;
+
+public class RunningTrack{
+
+    protected int distance;
+
+    public RunningTrack (int distance) {
+        this.distance = distance;
+    }
+
+    public int getDistance(){
+        return distance;
+    }
+
+
+}
